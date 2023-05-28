8b99b445cfe0876d233164ec3f832d65bb9d2f6e
Robert <soreterobert@gmail.com>
2023-05-28 14:28:11 +0300

    Movement done

--- cod3d.cpp
+++ cod3d.cpp
@@ -11,6 +11,21 @@ float lx = 0.0f, lz = -1.0f;
 // XZ position of the camera
 float x = 0.0f, z = 5.0f;
 
+// Strada
+const int TOTAL_LANES = 3;
+const int LANES[] = { -10, 0, 10 };
+int current_lane = 1;
+
+enum direction
+{
+    STANGA = -1,
+    DREAPTA = 1,
+    LOC = 0
+};
+direction directie = LOC; // directia deplasarii politistului, initial sta pe loc
+
+const float police_speed = 0.3f;
+
 void changeSize(int w, int h)
 {
     // Prevent a divide by zero, when window is too short
@@ -85,23 +100,87 @@ void renderScene(void)
     // Draw road
     glColor3f(0.2f, 0.2f, 0.2f);
     glBegin(GL_QUADS);
-    glVertex3f(-10.0f, 0.01f, -100.0f);
-    glVertex3f(-10.0f, 0.01f, 100.0f);
-    glVertex3f(10.0f, 0.01f, 100.0f);
-    glVertex3f(10.0f, 0.01f, -100.0f);
+    glVertex3f(-7.5f, 0.01f, -100.0f);
+    glVertex3f(-7.5f, 0.01f, 100.0f);
+    glVertex3f(7.5f, 0.01f, 100.0f);
+    glVertex3f(7.5f, 0.01f, -100.0f);
     glEnd();
 
-    drawSnowMan();
-    // // Draw 36 SnowMen
-    // for (int i = -3; i < 3; i++)
-    //     for (int j = -3; j < 3; j++)
-    //     {
-    //         glPushMatrix();
-    //         glTranslatef(i * 10.0, 0, j * 10.0);
-    //         drawSnowMan();
-    //         glPopMatrix();
-    //     }
+    // Draw road border
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_LINES);
+    glVertex3f(-7.5f, 0.015f, -100.0f);
+    glVertex3f(-7.5f, 0.015f, 100.0f);
+    glVertex3f(7.5f, 0.015f, -100.0f);
+    glVertex3f(7.5f, 0.015f, 100.0f);
+    glEnd();
+
+    // Enable line stippling
+    glEnable(GL_LINE_STIPPLE);
+
+    // Set line stipple pattern (0xAAAA pentru linie punctată)
+    glLineStipple(1, 0xAAAA);
+
+    // Set line width
+    glLineWidth(2.0f);
+
+    // Draw dashed line in the middle of the road
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_LINES);
+    float y = 0.016f;
+    float zStart = -100.0f;
+    float zEnd = 100.0f;
+    float zIncrement = 5.0f;
+
+    for (float z = zStart; z <= zEnd; z += zIncrement) {
+        glVertex3f(2.5f, y, z);
+        glVertex3f(2.5f, y, z + 2.0f);
+    }
+    for (float z = zStart; z <= zEnd; z += zIncrement) {
+        glVertex3f(-2.5f, y, z);
+        glVertex3f(-2.5f, y, z + 2.0f);
+    }
+
+    glEnd();
+
+    // Disable line stippling
+    glDisable(GL_LINE_STIPPLE);
+
+
+
+
+
+
+    // drawSnowMan();
+    // Movement politist
+
+    // Movement politist
+    if (directie == STANGA) {
+
+        if (x - police_speed < (current_lane - 1) * 5.0f) {
+            directie = LOC;
+            x = (current_lane - 1) * 5.0f;
+        }
+        else
+        {
+            x -= police_speed;
+        }
+    }
+    else if (directie == DREAPTA) {
+        if (x + police_speed > (current_lane - 1) * 5.0f) {
+            directie = LOC;
+            x = (current_lane - 1) * 5.0f;
+        }
+        else
+        {
+            x += police_speed;
+        }
+    }
+
+
+    glutPostRedisplay();
     glutSwapBuffers();
+    glFlush();
 }
 
 void processNormalKeys(unsigned char key, int x, int y)
@@ -109,52 +188,67 @@ void processNormalKeys(unsigned char key, int x, int y)
     float fraction = 0.1f;
     switch (key)
     {
-    case 'l':
-        angle -= 0.01f;
-        lx = sin(angle);
-        lz = -cos(angle);
-        break;
-    case 'a':
-        x -= lx * fraction;
-        break;
+        case 'l':
+            angle -= 0.01f;
+            lx = sin(angle);
+            lz = -cos(angle);
+            break;
 
-    case 'd':
-        x += 10;
-        break;
+        case 'a':   // stanga
+            x -= police_speed;
+            break;
+        case 'd':
+            x += police_speed;
+            break;
+        case 27:    // escape key
+            exit(0);
+            break;
     }
-    if (key == 27) // escape key
-        exit(0);
+}
+
+void moveRight()
+{
+    directie = DREAPTA;
+    current_lane = min(current_lane + 1, TOTAL_LANES - 1);
+
+    glutPostRedisplay();
+}
+
+void moveLeft()
+{
+    directie = STANGA;
+    current_lane = max(current_lane - 1, 0);
+
+    glutPostRedisplay();
 }
 
 void processSpecialKeys(int key, int xx, int yy)
 {
-
-    float fraction = 1.0f;
-    cout << angle << '\n';
     switch (key)
     {
-    case GLUT_KEY_UP:
-        angle -= 0.01f;
-        lx = sin(angle);
-        lz = -cos(angle);
-        break;
-    case GLUT_KEY_DOWN:
-        angle += 0.01f;
-        lx = sin(angle);
-        lz = -cos(angle);
-        break;
-    case GLUT_KEY_RIGHT:
-        x += lx * fraction;
-        z += lz * fraction;
-        break;
-    case GLUT_KEY_LEFT:
-        x -= lx * fraction;
-        z -= lz * fraction;
-        break;
+        case GLUT_KEY_UP:
+            angle -= 0.01f;
+            lx = sin(angle);
+            lz = -cos(angle);
+            break;
+        case GLUT_KEY_DOWN:
+            angle += 0.01f;
+            lx = sin(angle);
+            lz = -cos(angle);
+            break;
+        case GLUT_KEY_LEFT:
+            moveLeft();
+            break;
+        case GLUT_KEY_RIGHT:
+            moveRight();
+            break;
     }
+    cout << "X: " << x << "  Z: " << z << "  Angle: " << angle << '\n';
+    cout << current_lane << '\n';
+
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     // init GLUT and create window
     glutInit(&argc, argv);
@@ -172,6 +266,7 @@ int main(int argc, char **argv)
 
     // OpenGL init
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LINE_SMOOTH);
 
     // enter GLUT event processing cycle
     glutMainLoop();
